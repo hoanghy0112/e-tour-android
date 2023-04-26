6df53d7896281f80af62fa9e1fcb5ef7a8884075
Hoang Hy <hoanghy01122003@gmail.com>
2023-04-26 23:56:12 +0700

    feat: Add OnBoarding screen
    Temporarily navigate from MainActivity to OnBoardingActivity to test it, in the future, we'll use Shared Preferences to acknowledge whether it is the first time user install our app and then display on boarding screen if it's the first time

---
+++ app/src/main/java/com/teamone/e_tour/OnBoarding.java
@@ -0,0 +1,14 @@
+package com.teamone.e_tour;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OnBoarding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding);
+    }
+}
