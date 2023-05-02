8557bfd258c668e67e913281cc9551257c4066bd
Hoang Hy <hoanghy01122003@gmail.com>
2023-05-02 20:00:15 +0700

    feat: add navigation for book ticket and design checkout screen

---
+++ app/src/main/java/com/teamone/e_tour/fragments/BookTicketFragment.java
@@ -0,0 +1,33 @@
+package com.teamone.e_tour.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.teamone.e_tour.R;
+import com.teamone.e_tour.databinding.FragmentBookTicketBinding;
+
+public class BookTicketFragment extends Fragment {
+
+    public BookTicketFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        FragmentBookTicketBinding binding = FragmentBookTicketBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
+}
---
+++ app/src/main/java/com/teamone/e_tour/fragments/CheckoutFragment.java
@@ -0,0 +1,30 @@
+package com.teamone.e_tour.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.teamone.e_tour.R;
+
+public class CheckoutFragment extends Fragment {
+
+    public CheckoutFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_checkout, container, false);
+    }
+}
