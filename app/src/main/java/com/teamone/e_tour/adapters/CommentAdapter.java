c74b79e0bc91bd872bfbd6d7c32ae0c3addcf954
Hoang Hy <hoanghy01122003@gmail.com>
2023-05-08 15:35:34 +0700

    feat: finish CommentAdapter

--- app/src/main/java/com/teamone/e_tour/adapters/CommentAdapter.java
+++ app/src/main/java/com/teamone/e_tour/adapters/CommentAdapter.java
@@ -1,13 +1,58 @@
 package com.teamone.e_tour.adapters;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class CommentAdapter {
+import com.bumptech.glide.Glide;
+import com.teamone.e_tour.R;
+import com.teamone.e_tour.entities.Rating;
+
+import java.util.ArrayList;
+
+
+public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.ViewHolder> {
+    private Fragment context;
+    private ArrayList<Rating> ratings = new ArrayList<>();
+
+    public CommentAdapter(Fragment context) {
+        this.context = context;
+    }
+
+    public void setRatings(ArrayList<Rating> ratings) {
+        this.ratings = ratings;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_comment, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Rating rating = ratings.get(position);
+
+        holder.comment.setText(rating.getDescription());
+        holder.ratingPoint.setText(String.valueOf(rating.getStar()));
+        holder.userDisplayName.setText(rating.getUserId().getFullName());
+        Glide.with(context).load(rating.getUserId().getImage()).into(holder.userImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return ratings.size();
+    }
+
     class ViewHolder extends RecyclerView.ViewHolder {
         ImageView userImage;
         TextView userDisplayName;
@@ -17,6 +62,10 @@ public class CommentAdapter {
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            userImage = itemView.findViewById(R.id.user_image);
+            userDisplayName = itemView.findViewById(R.id.user_display_name);
+            ratingPoint = itemView.findViewById(R.id.rating_point);
+            comment = itemView.findViewById(R.id.comment);
         }
     }
 }
--- app/src/main/java/com/teamone/e_tour/fragments/DetailRouteFragment.java
+++ app/src/main/java/com/teamone/e_tour/fragments/DetailRouteFragment.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
 import android.util.Log;
@@ -19,14 +20,17 @@ import android.view.WindowManager;
 
 import com.bumptech.glide.Glide;
 import com.teamone.e_tour.R;
+import com.teamone.e_tour.adapters.CommentAdapter;
 import com.teamone.e_tour.adapters.DestinationAdapter;
 import com.teamone.e_tour.adapters.ImageAdapter;
 import com.teamone.e_tour.api.route.ViewDetailRouteApi;
 import com.teamone.e_tour.databinding.FragmentDetailRouteBinding;
 import com.teamone.e_tour.dialogs.LoadingDialog;
+import com.teamone.e_tour.entities.Rating;
 import com.teamone.e_tour.entities.TouristRoute;
 import com.teamone.e_tour.models.BookingDataManager;
 import com.teamone.e_tour.models.DetailRouteManager;
+import com.teamone.e_tour.models.RatingManager;
 import com.teamone.e_tour.utils.Formatter;
 
 import java.util.ArrayList;
@@ -46,6 +50,7 @@ public class DetailRouteFragment extends Fragment {
         super.onCreate(savedInstanceState);
         String routeId = getArguments().getString("id");
         DetailRouteManager.getInstance((AppCompatActivity) getActivity()).viewRoute(routeId);
+        RatingManager.getInstance((AppCompatActivity) getActivity()).viewRating(routeId);
     }
 
     @Override
@@ -54,7 +59,7 @@ public class DetailRouteFragment extends Fragment {
         FragmentDetailRouteBinding binding = FragmentDetailRouteBinding.inflate(inflater, container, false);
 
         getActivity().findViewById(R.id.bottom_navigation).setVisibility(View.INVISIBLE);
-        getActivity().findViewById(R.id.home_wrapper).setPadding(0,0, 0, 0);
+        getActivity().findViewById(R.id.home_wrapper).setPadding(0, 0, 0, 0);
 
         Window window = getActivity().getWindow();
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
@@ -85,6 +90,19 @@ public class DetailRouteFragment extends Fragment {
 
         LoadingDialog dialog = new LoadingDialog(getActivity());
         dialog.showLoading(getString(R.string.loading_tourist_route));
+
+        CommentAdapter adapter = new CommentAdapter(this);
+
+        binding.commentList.setAdapter(adapter);
+        binding.commentList.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
+
+        RatingManager.getInstance((AppCompatActivity) getActivity()).getRating().observe(getViewLifecycleOwner(), new Observer<ArrayList<Rating>>() {
+            @Override
+            public void onChanged(ArrayList<Rating> ratings) {
+                if (ratings != null)
+                    adapter.setRatings(ratings);
+            }
+        });
 
         DetailRouteManager.getInstance((AppCompatActivity) getActivity()).getRouteInfo().observe(getViewLifecycleOwner(), new Observer<TouristRoute>() {
             @Override
